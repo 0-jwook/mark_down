df345e1ed0dafe8d2221dc4472b0ca19443272ef
My-desiny-ae <tonnyk1130@gmail.com>
2025-06-01 00:39:00 +0900

    html으로 변환 하는 함수 생성

--- mark_down_front/src/app/editer.tsx
+++ mark_down_front/src/app/editer.tsx
@@ -3,14 +3,12 @@ import styled from "@emotion/styled";
 type Props = {
   value: string;
   onChange: (value: string) => void;
-  titleValue: string;
-  titleOnChange: (value: string) => void;
 }
 
-function Editer({titleValue, value, onChange} : Props) {
+function Editer({ value, onChange} : Props) {
   return(
     <div style={{display: "flex", flexDirection: "column"}}>
-      <StyledTextArea isTitle value={titleValue} ></StyledTextArea>
+      {/*<StyledTextArea isTitle></StyledTextArea>*/}
       <StyledTextArea value={value} onChange={e => onChange(e.target.value)}>
       </StyledTextArea>
     </div>
@@ -19,7 +17,7 @@ function Editer({titleValue, value, onChange} : Props) {
 
 const StyledTextArea = styled.textarea<{ isTitle?: boolean }>`
     width: 50vw;
-    height: ${({ isTitle }) => (isTitle ? "15vh" : "85vh")};
+    height: 100vh;
     background-color: ${({ isTitle }) => (isTitle ? "transparent" : "blue")};
     font-size: ${({ isTitle }) => (isTitle ? "30px" : "15px")};
     font-weight: ${({ isTitle }) => (isTitle ? "bold" : "normal")};
--- mark_down_front/src/app/preview.tsx
+++ mark_down_front/src/app/preview.tsx
@@ -1,16 +1,39 @@
-import remarkBreaks from 'remark-breaks';
-import ReactMarkdown from 'react-markdown';
 import styled from "@emotion/styled";
 
 type Props ={
   content: string
 }
 
+
+function parseMarkdown(text: string): string {
+  return text
+    // 제목 (# ~ ######)
+    .replace(/^###### (.*$)/gim, '<h6>$1</h6>')
+    .replace(/^##### (.*$)/gim, '<h5>$1</h5>')
+    .replace(/^#### (.*$)/gim, '<h4>$1</h4>')
+    .replace(/^### (.*$)/gim, '<h3>$1</h3>')
+    .replace(/^## (.*$)/gim, '<h2>$1</h2>')
+    .replace(/^# (.*$)/gim, '<h1>$1</h1>')
+    // 굵게 (**text** or __text__)
+    .replace(/\*\*(.*?)\*\*/gim, '<strong>$1</strong>')
+    .replace(/__(.*?)__/gim, '<strong>$1</strong>')
+    // 기울임 (*text* or _text_)
+    .replace(/\*(.*?)\*/gim, '<em>$1</em>')
+    .replace(/_(.*?)_/gim, '<em>$1</em>')
+    // 코드 (`code`)
+    .replace(/`([^`]+)`/gim, '<code>$1</code>')
+    // 링크 [text](url)
+    .replace(/\[([^\]]+)\]\(([^)]+)\)/gim, '<a href="$2" target="_blank">$1</a>')
+    // 줄바꿈
+    // .replace(/\n$/gim, '<br />');
+}
+
+
 function Preview({ content }: Props) {
   return (
     <div className="prose max-w-none p-4 overflow-y-auto">
       <StyledPreview>
-        <ReactMarkdown remarkPlugins={[remarkBreaks]} >{content}</ReactMarkdown>
+        {parseMarkdown(content)}
       </StyledPreview>
     </div>
   );
